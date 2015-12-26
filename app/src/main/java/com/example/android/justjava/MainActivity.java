43667607485d65446e3b4c2a334cbd4d727d2fbe
Uriel Carrillo <uriel.carrillo@caffenio.com>
2015-12-26 12:47:38 -0700

    added string to the result of the order, now it shows a total and a thank you message

--- app/src/main/java/com/example/android/justjava/MainActivity.java
+++ app/src/main/java/com/example/android/justjava/MainActivity.java
@@ -24,21 +24,20 @@ public class MainActivity extends ActionBarActivity {
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-
-        display(quantity);
-        displayPrice(quantity * 5);
+        String priceMessage = "Total = $";
+        displayMessage(priceMessage+quantity*5);
     }
 
     public void increment(View view) {
         quantity=quantity+1;
         display(quantity);
-        displayPrice(quantity*5);
+
     }
 
     public void decrement(View view) {
         quantity=quantity-1;
         display(quantity);
-        displayPrice(quantity*5);
+
     }
 
     /**
@@ -56,5 +55,14 @@ public class MainActivity extends ActionBarActivity {
     private void displayPrice(int number) {
         TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance(Locale.US).format(number));
+    }
+
+    /**
+     * This method displays the given text on the screen.
+     */
+    private void displayMessage(String message) {
+        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        message= message+"\n Thank You!";
+        priceTextView.setText(message);
     }
 }
